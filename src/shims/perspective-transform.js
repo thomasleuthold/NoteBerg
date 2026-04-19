1092be7608ef5f584ba68e1bea97d0532016a0ef
Thomas <thomas_leuthold@hotmail.com>
2026-04-19 22:41:51 +0200

    Fix formatting

--- src/shims/perspective-transform.js
+++ src/shims/perspective-transform.js
@@ -3,10 +3,10 @@
 // Tauri CSP includes unsafe-eval so new Function is allowed at runtime.
 // For NC builds, vite.config.js replaces this entire module at build time.
 
-import src from '../../node_modules/perspective-transform/dist/perspective-transform.js?raw';
+import src from "../../node_modules/perspective-transform/dist/perspective-transform.js?raw";
 
 const mod = { exports: {} };
 // eslint-disable-next-line no-new-func
-new Function('root', 'module', 'exports', src)({}, mod, mod.exports);
+new Function("root", "module", "exports", src)({}, mod, mod.exports);
 
 export default mod.exports;
